906c73fe5c6f14748687454d6d9bfe5c449b3bc2
Seooyooungg <151813830+Seooyooungg@users.noreply.github.com>
2024-02-12 23:00:13 +0900

    board

---
+++ demo/src/main/java/com/study/demo/entity/Board.java
@@ -0,0 +1,23 @@
+package com.study.demo.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+
+    private String content;
+}
---
+++ demo/src/main/java/com/study/demo/repository/BoardRepository.java
@@ -0,0 +1,9 @@
+package com.study.demo.repository;
+
+import com.study.demo.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Integer> {
+}
